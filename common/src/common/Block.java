b9ebcf91c6682132479f23c73d05b3289cb0182e
Aaron Chelvan <aaron.chelvan@outlook.com>
2019-05-17 19:25:54 +1000

    Added method for computing transaction ID

---
+++ common/src/common/Block.java
@@ -0,0 +1,17 @@
+package common;
+
+import java.util.ArrayList;
+
+public class Block {
+	private static final int maxNumTransactions = 1500; // Maximum number of transactions in a block
+	private ArrayList<Transaction> transactionList;
+	private String blockId;
+	private String prevBlockId;
+	
+	public Block() {
+		transactionList = new ArrayList<Transaction>();
+		blockId = "";
+		prevBlockId = "";
+	}
+	
+}
--- common/src/common/Transaction.java
+++ common/src/common/Transaction.java
@@ -1,4 +1,6 @@
 package common;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
@@ -6,20 +8,41 @@ public class Transaction {
 	
 	private String timestamp; // Timestamp of creation
 	private String tid; // Transaction ID
-	private String prev_tid; // Previous transaction ID
+	private String prevTid; // Previous transaction ID
 	private String input;
 	private String output;
 	private String signature;
-	private String pub_key;
+	private String pubKey;
 	
 	public Transaction() {
 		timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
-		tid = UUID.randomUUID().toString();
-		prev_tid = UUID.randomUUID().toString();
+		tid = "";
+		prevTid = "";
 		input = "";
 		output = "";
 		signature = "";
-		pub_key = "";
+		pubKey = "";
+	}
+	
+	// Compute the transaction ID by hashing the contents of the transaction
+	public String generateTid() {
+		MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance("SHA-256");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		md.update(timestamp.getBytes());
+		md.update(prevTid.getBytes());
+		md.update(input.getBytes());
+		md.update(output.getBytes());
+		md.update(signature.getBytes());
+		md.update(pubKey.getBytes());
+		return new String(md.digest());
+	}
+	
+	public void setPrevTid(String prevTid) {
+		this.prevTid = prevTid;
 	}
 	
 	
