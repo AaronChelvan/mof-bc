19a4455f7a8e3fc8a42ab49bf00407e43754629c
Aaron Chelvan <aaron.chelvan@outlook.com>
2019-05-26 01:01:06 +1000

    Node can communicate with miner via socket

--- miner/src/Miner.java
+++ miner/src/Miner.java
@@ -1,10 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 
 public class Miner {
 
-	public static void main(String[] args) {
-		ArrayList<Block> blockchain = new ArrayList<Block>();
+	public static void main(String[] args) throws Exception {
 		System.out.println("I am a Miner");
+		ArrayList<Block> blockchain = new ArrayList<Block>();
+		ServerSocket minerSocket = new ServerSocket(8000);
+		
+		while (true) {
+			Socket connectionSocket = minerSocket.accept();
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+		    String clientSentence = inFromClient.readLine();
+		    System.out.println(clientSentence);
+		}
 	}
 
 }
--- node/src/Node.java
+++ node/src/Node.java
@@ -1,8 +1,16 @@
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class Node {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		System.out.println("I am a Node");
+		Socket clientSocket = new Socket("localhost", 8000);
+		OutputStream output = clientSocket.getOutputStream();
+		PrintWriter writer = new PrintWriter(output, true);
+		writer.println("This is a message from the node");
+		clientSocket.close();
 	}
 	
 }
