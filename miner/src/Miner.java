6fd774dd41b06b29deab5877361fd2939df14c41
Aaron Chelvan <aaron.chelvan@outlook.com>
2019-06-19 19:23:14 +1000

    Added search agent to docker-compose file. Edited Miner to remove arraylist containing blocks.

--- miner/src/Miner.java
+++ miner/src/Miner.java
@@ -12,18 +12,20 @@ public class Miner {
 	public static void main(String[] args) throws Exception {
 		System.out.println("Miner is running");
 		int maxBlockchainSize = 100;
+		int numBlocks = 0;
 		
 		// LevelDB setup
 		Options options = new Options();
 		options.createIfMissing(true);
 		DB db = factory.open(new File("blockchain"), options);
 		
-		ArrayList<Block> blockchain = new ArrayList<Block>();
+		// Blockchain configuration
 		Block currentBlock = new Block();
 		String prevTransactionId = "";
 		String prevBlockId = "";
 		String currentBlockId = "";
 		
+		// Socket setup
 		ServerSocket minerSocket = new ServerSocket(8000);
 		
 		while (true) {
@@ -40,6 +42,7 @@ public class Miner {
 			currentBlock.addTransaction(t);
 			if (currentBlock.isFull()) {
 				System.out.println("Block filled");
+				System.out.println(Util.socketClientName(connectionSocket));
 				
 				// Set the prevBlockId and blockId fields
 				currentBlock.setPrevBlockId(prevBlockId);
@@ -47,10 +50,16 @@ public class Miner {
 				currentBlockId = currentBlock.getBlockId();
 				
 				// Add the block to the blockchain
-				db.put(bytes(currentBlockId), Util.blockToBytes(currentBlock));
+				db.put(bytes(currentBlockId), Util.serialize(currentBlock));
+				numBlocks++;
+				
+				// Checks if a key does not exist
+				/*if (db.get(bytes("qwioeiugryf")) == null) {
+					System.out.println("key does not exist");
+				}*/
 				
 				// Stop running once a certain amount of blocks have been added to the blockchain
-				if (blockchain.size() >= maxBlockchainSize) {
+				if (numBlocks >= maxBlockchainSize) {
 					minerSocket.close();
 					return;
 				}
