49d95013a262ef7b94b1b77ed05c3d6bb3aa6f94
Aaron Chelvan <aaron.chelvan@outlook.com>
2019-06-19 16:56:24 +1000

    Made a function that identifies which client connected to a server socket

--- common/src/Util.java
+++ common/src/Util.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Util {
 	
@@ -21,6 +24,30 @@ public class Util {
 		ByteArrayInputStream bis = new ByteArrayInputStream(data);
 		ObjectInputStream in = new ObjectInputStream(bis);
 		return (Block) in.readObject();
+	}
+	
+	// Given a socket, return the name of the client connected to that socket
+	public static String socketClientName(Socket s) throws UnknownHostException {
+		// IP addresses of the nodes in the network
+		String node1IP = InetAddress.getByName("node1").getHostAddress();
+		String node2IP = InetAddress.getByName("node2").getHostAddress();
+		String miner1IP = InetAddress.getByName("miner1").getHostAddress();
+		System.out.println(miner1IP);
+		
+		// Get the IP address of the client
+		String clientIP = s.getRemoteSocketAddress().toString();
+		clientIP = clientIP.substring(1); // Strip the leading "/"
+		clientIP = clientIP.split(":")[0]; // Ignore the port. We only want the IP address.
+		
+		if (clientIP.equals(node1IP)) {
+			return "node1";
+		} else if (clientIP.equals(node2IP)) {
+			return "node2";
+		} else if (clientIP.equals(miner1IP)){
+			return "miner1";
+		} else {
+			return "";
+		}
 	}
 
 }
