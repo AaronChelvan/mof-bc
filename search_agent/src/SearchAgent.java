daca93e82d83645fa305c84801f1ac450467edcf
Aaron Chelvan <aaron.chelvan@outlook.com>
2019-06-21 15:20:19 +1000

    Search agent finds remove transactions, removes them from the blockchain, and transmits the updated blocks back to the miner

--- search_agent/src/SearchAgent.java
+++ search_agent/src/SearchAgent.java
@@ -4,9 +4,13 @@ import static org.fusesource.leveldbjni.JniDBFactory.factory;
 import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -17,6 +21,7 @@ import org.iq80.leveldb.Options;
 
 public class SearchAgent {
 	private static DB db; // LevelDB database used for storing the blockchain
+	private static HashMap<String, Block> updatedBlocks; // Key = block ID. Val = block.
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		// LevelDB setup
@@ -32,8 +37,10 @@ public class SearchAgent {
 		Runnable blockchainScan = new Runnable() {
 			public void run() {
 				try {
-					searchBlockchain();
-				} catch (ClassNotFoundException | IOException e) {
+					transmitUpdatedBlocks();
+				} catch (UnknownHostException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
@@ -41,55 +48,77 @@ public class SearchAgent {
 		ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 		executor.scheduleAtFixedRate(blockchainScan, 0, cleaningPeriod, TimeUnit.SECONDS);
 		
+		// A list of blocks that have been updated over the last cleaning period
+		updatedBlocks = new HashMap<String, Block>(); // Key = block ID. Val = block.
+		
 		System.out.println("Setup complete");
 		
 		// Listen for incoming connections
 		while (true) {
-			// Receive Blocks from the miner or one of the agents
+			// Receive Blocks from the miner
 			Socket connectionSocket = agentSocket.accept();
 			ObjectInputStream in = new ObjectInputStream(connectionSocket.getInputStream());
 			Block currentBlock = (Block) in.readObject();
 			
-			if (Util.socketClientName(connectionSocket).equals("miner1")) {
-				// Scan the block for remove transactions & summary transactions
-				for (Transaction t: currentBlock.getTransactions()) {
-					if (t.getType() == TransactionType.Remove) {
-						// Extract the location of the transaction to be removed
-						TransactionLocation tl = Util.deserialize(bytes(t.getData()));
-						
-						// Get the block containing the transaction
-						Block b = Util.deserialize(db.get(bytes(tl.getBlockID())));
-						
-						// Find the transaction to be removed
-						for (Transaction t2: b.getTransactions()) {
-							if (t2.getTid().equals(tl.getTransactionID())) {
-								// Delete everything in this transaction except for the transaction ID and previous transaction ID
-								t2.clearTransaction();
-								break;
-							}
-						} 
-						
-						// Put the updated block back in the database
-						db.put(bytes(b.getBlockId()), Util.serialize(b));
-						
-					} else if (t.getType() == TransactionType.Summary) {
-						// TODO
-						
+			if (!Util.socketClientName(connectionSocket).equals("miner1")) {
+				System.out.println("Something went wrong!");
+			}
+			
+			// Scan the block for remove transactions & summary transactions
+			for (Transaction t: currentBlock.getTransactions()) {
+				if (t.getType() == TransactionType.Remove) {
+					// Extract the location of the transaction to be removed
+					TransactionLocation tl = Util.deserialize(bytes(t.getData()));
+					
+					// If the block to be updated is not already in updatedBlocks list,
+					// get it from the database
+					Block b;
+					if (!updatedBlocks.containsKey(tl.getBlockID())) {
+						b = Util.deserialize(db.get(bytes(tl.getBlockID())));
+					} else {
+						b = updatedBlocks.get(tl.getBlockID());
 					}
+					
+					// Find the transaction to be removed
+					for (Transaction t2: b.getTransactions()) {
+						if (t2.getTid().equals(tl.getTransactionID())) {
+							// Delete everything in this transaction except for the transaction ID and previous transaction ID
+							t2.clearTransaction();
+							break;
+						}
+					}
+					
+				} else if (t.getType() == TransactionType.Summary) {
+					// TODO
+					
 				}
-			} else {
-				//TODO - if the client is an agent, then this is an updated block
-				//Check that this block already exists in the blockchain
-				System.out.println("Something went wrong");
 			}
 			
 			// Add the block to the blockchain
 			db.put(bytes(currentBlock.getBlockId()), Util.serialize(currentBlock));
+			
 		}
 	}
 	
-	private void transmitUpdatedBlock(Block b) {
+	private static void transmitUpdatedBlocks() throws UnknownHostException, IOException {
+		// Establish a connection to the miner
+		Socket clientSocket = new Socket("miner1", 8000);
+		ObjectOutputStream output = new ObjectOutputStream(clientSocket.getOutputStream());
 		
+		for (Block b : updatedBlocks.values()) {
+			// Transmit the updated blocks to the miner
+			output.writeObject(b);
+			
+			// Write the updated blocks to the database
+			db.put(bytes(b.getBlockId()), Util.serialize(b));
+		}
+		System.out.println("Transmitted updated blocks");
+		
+		// Clear the list of updated blocks
+		updatedBlocks.clear();
+		
+		// Close the connection
+		clientSocket.close();
 	}
 
 }
