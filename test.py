98e56716153a04d90eb8db67246a7f9c8af283a8
Aaron Chelvan <aaron.chelvan@outlook.com>
2019-11-10 16:16:03 +1100

    Updated test script

--- test.py
+++ test.py
@@ -1,31 +1,23 @@
+#!/usr/bin/python
 import sys, os, plyvel
 
 if len(sys.argv) != 2:
 	print("Wrong number of args")
 	exit()
 
-if sys.argv[1] == "before":
-	os.system("cp -a blockchain_copies/node_blockchain/ analysis/before/")
-elif sys.argv[1] == "after":
-	os.system("cp -a blockchain_copies/node_blockchain/ analysis/after/")
-#elif sys.argv[1] == "before2":
-#	os.system("cp -a blockchain_copies/node_blockchain/ analysis/before2/")
-#elif sys.argv[1] == "after2":
-#	os.system("cp -a blockchain_copies/node_blockchain/ analysis/after2/")
-elif sys.argv[1] == "clear":
-	os.system("rm -rf analysis/after/*")
-	os.system("rm -rf analysis/before/*")
+if sys.argv[1] == "clear": # Delete the blockchains and their copies
+	os.system("rm -rf analysis/*")
 	os.system("sudo rm -rf blockchain_copies/*")
-elif sys.argv[1] == "compare":
-	db_list = ["./analysis/before/node_blockchain", "./analysis/after/node_blockchain"]
-	#db_list = ["./analysis/before/node_blockchain", "./analysis/after/node_blockchain", "./analysis/before2/node_blockchain", "./analysis/after2/node_blockchain"]
+elif sys.argv[1] == "compare": # Compare all of the blockchains in the "analysis" folder
+	db_list = os.listdir("./analysis/")
 
 	for database in db_list:
-		db = plyvel.DB(database)
+		print(database)
+		db = plyvel.DB("./analysis/" + database)
 
 		total = 0
 		for key, val in db:
-			print(repr(val))
+			#print(repr(val))
 			total += len(val)
 		
 		print("Size = %d" % total)
@@ -33,6 +25,7 @@ elif sys.argv[1] == "compare":
 		sizes = []
 		for key, val in db:
 			sizes.append(len(val))
-		print("\n" + str(sizes) + ". Num blocks = " + str(len(sizes)) + "\n")	
-else:
-	print("Invalid arg")
+		print("\n" + str(sizes) + ". Num blocks = " + str(len(sizes)) + "\n")
+else: # Copy the node's blockchain to the analysis folder using the given argument as the folder name
+	folder = sys.argv[1]
+	os.system("cp -a blockchain_copies/node_blockchain/ analysis/%s/" % (folder))
